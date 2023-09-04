4622f8add6a5b68adc709ac7d6b50f22a9dff2ac
DoubleZ <mihailagei2019@mail.ru>
2023-09-04 20:48:03 +0300

    Most part is done

--- server/main.php
+++ server/main.php
@@ -1,39 +1,41 @@
-<?php session_start();
-function isInFirst($r, $x, $y) {
-    return ($x >= 0 && $y >= 0 && ($x**2 + $y**2 <= ($r / 2) ** 2));
+<?php
+function isInFirst($r, $x, $y)
+{
+    return ($x >= 0 && $y >= 0 && ($x ** 2 + $y ** 2 <= ($r / 2) ** 2));
 }
 
-function isInSecond($r, $x, $y) {
-    return ($x <= 0 && $y >= 0 && $x >= -$r/2 && $y <= $r);
+function isInSecond($r, $x, $y)
+{
+    return ($x <= 0 && $y >= 0 && $x >= -$r / 2 && $y <= $r);
 }
 
-function isInFourth($r, $x, $y) {
+function isInFourth($r, $x, $y)
+{
     return ($x <= 0 && $y >= 0 && $y >= (-$r + 2 * $r * $x));
 }
 
-error_reporting(0);
-
 try {
+
     $time_start = microtime(true);
 
     $r = 0;
     $x = 0;
     $y = 0;
-    if(isset($_POST["r"])){
+    if (isset($_POST["r"])) {
 
         $r = $_POST["r"];
     }
-    if(isset($_POST["x"])){
+    if (isset($_POST["x"])) {
 
         $x = $_POST["x"];
     }
-    if(isset($_POST["y"])){
+    if (isset($_POST["y"])) {
 
         $y = $_POST["y"];
     }
 
     $isInside = (isInFirst($r, $x, $y) || isInSecond($r, $x, $y) || isInFourth($r, $x, $y));
-
+//    echo $isInside;
 
     date_default_timezone_set('Europe/Moscow');
     $currentTime = date('m/d/Y h:i:s a', time());
@@ -42,42 +44,97 @@ try {
     $time_end = microtime(true);
     $scriptWorkTime = $time_end - $time_start;
 
-    $rowsCount = ($_SESSION["rows"] != null ? count($_SESSION["rows"]) : 0);
+    $rowsCount = (isset($_COOKIE["rows"]) ? count($_COOKIE["rows"]) : 0);
+    echo $rowsCount;
+//    $rowsCount = 0;
 
-    $_SESSION = array();
-    $_SESSION["rows"]["r.$rowsCount"]['r'] = $r;
-    $_SESSION["rows"]["r.$rowsCount"]['x'] = $x;
-    $_SESSION["rows"]["r.$rowsCount"]['y'] = $y;
-    $_SESSION["rows"]["r.$rowsCount"]['isInside'] = $isInside;
-    $_SESSION["rows"]["r.$rowsCount"]['currentTime'] = $currentTime;
-    $_SESSION["rows"]["r.$rowsCount"]['scriptWorkTime'] = $scriptWorkTime;
-//    echo $_SESSION["rows[r.$rowsCount]['r']"];
+    $rowName = "r.$rowsCount";
+//    setcookie("rows[$rowName][r]", $r);
+//    setcookie("rows[$rowName][x]", $x);
+//    setcookie("rows[$rowName][y]", $y);
+//    setcookie("rows[$rowName][isInside]", $isInside);
+//    setcookie("rows[$rowName][currentTime]", $currentTime);
+//    setcookie("rows[$rowName][scriptWorkTime]", $scriptWorkTime);
+
+    $isInsideSign = $isInside ? "&#10004": "&#10008";
+
+    $array = array("r" => $r, "x" => $x, "y" => $y, "isInside" => $isInsideSign, "currentTime" => $currentTime, "scriptWorkTime" => $scriptWorkTime);
+    $serializedArray = serialize($array);
+    $key = "rows[$rowName]";
+    setcookie($key, $serializedArray);
+//    foreach (session_get_cookie_params() as $param){echo $param;};
 
 
     /** Создание html кода таблицы и дополнительных данных*/
-    $result = "<div>";
+
+
+    $result = "<!DOCTYPE html><html><head>
+<link rel='stylesheet' type='text/css' href='styles/table.css'>
+<link rel='stylesheet' type='text/css' href='styles/main.css'>
+</head><body><div class='container'>";
+    $goToMain = "<a href='../client/index.html' class='modern'>Вернуться на главную страницу</a>";
+    $result .= $goToMain;
 
     $table = "<table><thead><tr><th>R</th><th>X</th><th>Y</th><th>Is inside</th><th>Current time</th><th>Script Work Time</th>
 </tr></thead><tbody>";
-    foreach ($_SESSION["rows"] as $rowIndex => $array){
+    foreach ($_COOKIE["rows"] as $rowName => $serializedArray) {
         $table .= "<tr>";
-        foreach ($array as $elIndex => $elValue){
-            $table.= "<td>$elValue</td>";
+        $array = unserialize($serializedArray);
+        foreach ($array as $elIndex => $elValue) {
+            $table .= "<td>$elValue</td>";
         }
-        $table.= "</tr>";
+        $table .= "</tr>";
     }
+    $table.= "<td>$r</td><td>$x</td><td>$y</td><td>$isInsideSign</td><td>$currentTime</td><td>$scriptWorkTime</td>";
+
     $table .= "</tbody></table>";
 
     $result .= $table;
+    $result .= "</div></body></html>";
 
-    $result .= "</div>";
+    echo($result);
 
-    header('Content-Type: application/html');
-    echo json_encode($response);
-
-    echo $result;
-} catch (Error $ex){
-    echo ($ex);
+} catch (Error $ex) {
+    echo($ex);
 }
-
 ?>
+
+<!--<!DOCTYPE html>-->
+<!--<html lang='ru'>-->
+<!--<head>-->
+<!--    <meta charset='utf-8'>-->
+<!--    <title>Подключение css 3-мя способами!</title>-->
+<!--    <link rel="stylesheet" type="text/css" href="styles/table.css">-->
+<!--</head>-->
+<!---->
+<!--<body>-->
+<!--    <div>-->
+<!--    <table>-->
+<!--        <thead>-->
+<!--        <tr>-->
+<!--            <th>R</th>-->
+<!--            <th>X</th>-->
+<!--            <th>Y</th>-->
+<!--            <th>Is inside</th>-->
+<!--            <th>Current time</th>-->
+<!--            <th>Script Work Time</th>-->
+<!--        </tr>-->
+<!--        </thead>-->
+<!--        <tbody>-->
+<!--        --><?php
+//        foreach ($_COOKIE["rows"] as $rowIndex => $array) {
+//            echo "<tr>";
+//            foreach ($array as $elIndex => $elValue) {
+//                echo "<td>$elValue</td>";
+//            }
+//            echo "</tr>";
+//        }
+//        ?>
+<!--        </tbody>-->
+<!--    </table>-->
+<!---->
+<!--</div>-->
+<!--</body>-->
+<!--</html>-->
+
+
