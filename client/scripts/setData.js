f5a3820fee3571d93ed360799e49ad59156527e1
DoubleZ <mihailagei2019@mail.ru>
2023-09-05 18:08:40 +0300

    Right work with session

--- client/scripts/setData.js
+++ client/scripts/setData.js
@@ -1,5 +1,16 @@
-const now = new Date();
-const timeInput = document.getElementsByName("currentTime")[0]
-timeInput.value = now.toString();
+// const now = new Date();
+// const timeInput = document.getElementsByName("currentTime")[0]
+// timeInput.value = now.toString();
+//
+//
+// console.log(timeInput.value, now)
 
-console.log(timeInput.value, now)
+
+let input = document.createElement("input");
+input.name = "currentTime";
+document.getElementById('form').appendChild(input);
+input.disabled;
+// input.innerText = "Текущий часовой пояс";
+input.value = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+// <p name="currentTime">Текущий часовой пояс</p>>
--- server/main.php
+++ server/main.php
@@ -1,4 +1,4 @@
-<?php
+<?php session_start();
 function isInFirst($r, $x, $y)
 {
     return ($x >= 0 && $y >= 0 && ($x ** 2 + $y ** 2 <= ($r / 2) ** 2));
@@ -34,57 +34,66 @@ try {
         $y = $_POST["y"];
     }
 
-    $isInside = (isInFirst($r, $x, $y) || isInSecond($r, $x, $y) || isInFourth($r, $x, $y));
-    $isInsideSign = $isInside ? "&#10004": "&#10008";
+    if (isset($_POST["r"]) && isset($_POST["x"]) && isset($_POST["y"])){
+        $isInside = (isInFirst($r, $x, $y) || isInSecond($r, $x, $y) || isInFourth($r, $x, $y));
+        $isInsideSign = $isInside ? "&#10004": "&#10008";
 
-    $time = new DateTime();
-    date_default_timezone_set('Europe/Moscow');
+        $time = new DateTime();
+//        echo $_POST["currentTime"];
+        date_default_timezone_set($_POST["currentTime"]);
 //    $currentTime = date('m/d/Y h:i:s a', time());
-    $currentTime = (isset($_POST["currentTime"]) && strlen($_POST["currentTime"]) > 0) ? $_POST["currentTime"] : date('m/d/Y h:i:s a', time());;
+        $currentTime = date('m/d/Y h:i:s a', time());
 
-    $time_end = microtime(true);
-    $scriptWorkTime = $time_end - $time_start;
+        $time_end = microtime(true);
+        $scriptWorkTime = $time_end - $time_start;
 
-    $rowsCount = (isset($_COOKIE["rows"]) ? count($_COOKIE["rows"]) : 0);
-    $rowName = "r.$rowsCount";
+        $rowsCount = (isset($_SESSION["rows"]) ? count($_SESSION["rows"]) : 0);
+        $rowName = "r.$rowsCount";
 
-
-
-    $array = array("r" => $r, "x" => $x, "y" => $y, "isInside" => $isInsideSign, "currentTime" => $currentTime, "scriptWorkTime" => $scriptWorkTime);
-    $serializedArray = serialize($array);
-    $key = "rows[$rowName]";
-    setcookie($key, $serializedArray);
-//    foreach (session_get_cookie_params() as $param){echo $param;};
+        if ($rowsCount == 0){
+            $_SESSION["rows"] = array();
+        }
+        $array = array("r" => $r, "x" => $x, "y" => $y, "isInside" => $isInside, "currentTime" => $currentTime, "scriptWorkTime" => $scriptWorkTime);
+        $_SESSION["rows"][$rowName] = $array;
 
 
-    /** Создание html кода таблицы и дополнительных данных*/
+        /** Создание html кода таблицы и дополнительных данных*/
 
 
-    $result = "<!DOCTYPE html><html><head>
+        $result = "<!DOCTYPE html><html><head>
 <link rel='stylesheet' type='text/css' href='styles/table.css'>
 <link rel='stylesheet' type='text/css' href='styles/main.css'>
 </head><body><div class='container'>";
-    $goToMain = "<a href='../client/index.html' class='modern'>Вернуться на главную страницу</a>";
-    $result .= $goToMain;
+        $goToMain = "<a href='../client/index.html' class='modern'>Вернуться на главную страницу</a>";
+        $result .= $goToMain;
 
-    $table = "<table><thead><tr><th>R</th><th>X</th><th>Y</th><th>Is inside</th><th>Current time</th><th>Script Work Time</th>
+        $table = "<table><thead><tr><th>R</th><th>X</th><th>Y</th><th>Is inside</th><th>Current time</th><th>Script Work Time</th>
 </tr></thead><tbody>";
-    foreach ($_COOKIE["rows"] as $rowName => $serializedArray) {
-        $table .= "<tr>";
-        $array = unserialize($serializedArray);
-        foreach ($array as $elIndex => $elValue) {
-            $table .= "<td>$elValue</td>";
+
+        foreach ($_SESSION["rows"] as $rowIndex => $array){
+            $table .= "<tr>";
+            foreach ($array as $elIndex => $elValue){
+                $table.= "<td>$elValue</td>";
+            }
+            $table.= "</tr>";
         }
-        $table .= "</tr>";
-    }
-    $table.= "<td>$r</td><td>$x</td><td>$y</td><td>$isInsideSign</td><td>$currentTime</td><td>$scriptWorkTime</td>";
 
-    $table .= "</tbody></table>";
+//    foreach ($_COOKIE["rows"] as $rowName => $serializedArray) {
+//        $table .= "<tr>";
+//        $array = unserialize($serializedArray);
+//        foreach ($array as $elIndex => $elValue) {
+//            $table .= "<td>$elValue</td>";
+//        }
+//        $table .= "</tr>";
+//    }
+        $table.= "<td>$r</td><td>$x</td><td>$y</td><td>$isInsideSign</td><td>$currentTime</td><td>$scriptWorkTime</td>";
 
-    $result .= $table;
-    $result .= "</div></body></html>";
+        $table .= "</tbody></table>";
 
-    echo($result);
+        $result .= $table;
+        $result .= "</div></body></html>";
+
+        echo($result);
 
 //    $doc = new DOMDocument();
 //    $doc->loadHTML($result);
@@ -94,6 +103,11 @@ try {
 //    $doc = new DOMDocument;
 //
 //    $newnode = $doc->appendChild(htmlspecialchars_decode($result));
+    } else {
+        echo("<h1>Incorrect Data</h1>");
+    }
+
+
 
 
 } catch (Error $ex) {
