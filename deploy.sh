e8a9abde7aa9e57fedce8949f360dd2dea950819
Intention man <74947150+MishailAJ@users.noreply.github.com>
2023-10-03 13:31:18 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 scp -P 2222 -r "./client" "./server" s367044@helios.se.ifmo.ru:~/public_html
-
-#sudo rsync -av --recursive -e "ssh -i ./aitip_host_pass" ./build/ aitipms7_456@aitipms7.beget.tech:~/
